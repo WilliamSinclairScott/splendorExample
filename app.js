38dea37103314b57102eeea387e5233343ce903f
WilliamSinclairScott <william.sinclair.scott@gmail.com>
2024-03-18 15:49:10 -0400

    start init function for gameplay

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ const colorToClass = {
     black : 'colorB'
 }
 
-
+const cardAreaZone3 = document.getElementById('Level3Zone')
+const cardAreaZone2 = document.getElementById('Level2Zone')
+const cardAreaZone1 = document.getElementById('Level2Zone')
+const nobleArea = document.getElementById('NobleZone')
 
 //
 class Game {
@@ -45,12 +48,12 @@ class Game {
     }
     /**
      * Initializes available tokens for the game based on the number of players
-     * @param {number} numberOfPlayers 2,3,4
+     * 
      */
     setPlayerCountInit = () => {
 
-        if (this.numberOfPlayers === 2) {
-            this.playerCount = numberOfPlayers;
+        if (this.players.length == 2) {
+            this.this.players.length = this.players.length;
             this.tokens = {
                 Green: 4,
                 Blue: 4,
@@ -60,8 +63,8 @@ class Game {
                 Yellow: 5
             }
         }
-        else if (this.numberOfPlayers === 3) {
-            this.playerCount = numberOfPlayers;
+        else if (this.players.length == 3) {
+            this.playerCount = this.players.length;
             this.tokens = {
                 Green: 5,
                 Blue: 5,
@@ -71,8 +74,8 @@ class Game {
                 Yellow: 5
             }
         }
-        else if (this.numberOfPlayers === 4) {
-            this.playerCount = numberOfPlayers;
+        else if (this.players.length == 4) {
+            this.playerCount = this.players.length;
 
         }
         else {
@@ -103,7 +106,9 @@ class Game {
             throw new Error('Invalid level of card!');
         }
     }
-
+    dealNewNobleCard = () => {
+        return this.nobleDeck.pop
+    }
 }
 
 class Player {
@@ -229,8 +234,7 @@ class Player {
         this.victoryPoints += cardObject.pv
     }
 }
-/*
-Thinking out sudo code logic
+/* Thinking out sudo code logic
 
 Player.actions
     Click Gold(pile)
@@ -276,17 +280,16 @@ Player.cleanup
 // console.log(level3Objects);
 // console.log(nobleObjects);
 const testGame = new Game(level1Objects,level2Objects, level3Objects, nobleObjects)
-console.log(testGame)
-console.log(Game);
+
+// console.log(testGame)
+// console.log(Game);
+
 const testPlayer1 = new Player(`William`,12345)
 const testPlayer2 = new Player(`Callum`,23456)
 const testPlayer3 = new Player(`Lily`,34657)
 const testPlayer4 = new Player(`Sara`,45678)
 
-const cardAreaZone3 = document.getElementById('Level3Zone')
-const cardAreaZone2 = document.getElementById('Level2Zone')
-const cardAreaZone1 = document.getElementById('Level2Zone')
-const nobleArea = document.getElementById('NobleZone')
+initateGame(testGame)
 
 
 //------------------------functions below----------------------------------------
@@ -295,34 +298,99 @@ function initateGame(gameObject) {
         //NAMESPACE for said action
     
     //Demo material we gonna make 4 players play demo
-    testGame.addPlayer(testPlayer1)
-    testGame.addPlayer(testPlayer2)
-    testGame.addPlayer(testPlayer3)
-    testGame.addPlayer(testPlayer4)
-
-    testGame.setPlayerCountInit()
+    gameObject.addPlayer(testPlayer1)
+    gameObject.addPlayer(testPlayer2)
+    gameObject.addPlayer(testPlayer3)
+    gameObject.addPlayer(testPlayer4)
+    gameObject.setPlayerCountInit()
     
     //create new lvl1 cards
-    createNewCardElement(testGame.dealNewCardlevel(1))
-    createNewCardElement(testGame.dealNewCardlevel(1))
-    createNewCardElement(testGame.dealNewCardlevel(1))
-
+    while(cardAreaZone1.childElementCount != 3){
+        cardAreaZone1.appendChild(createNewCardElement(gameObject.dealNewCardlevel(1)))
+    }
     //create new lvl2 cards
-    createNewCardElement(testGame.dealNewCardlevel(2))
-    createNewCardElement(testGame.dealNewCardlevel(2))
-    createNewCardElement(testGame.dealNewCardlevel(2))
-
+    while(cardAreaZone2.childElementCount != 3){
+        cardAreaZone2.appendChild(createNewCardElement(gameObject.dealNewCardlevel(2)))
+        }
     //create new lvl3 cards
-    createNewCardElement(testGame.dealNewCardlevel(3))
-    createNewCardElement(testGame.dealNewCardlevel(3))
-    createNewCardElement(testGame.dealNewCardlevel(3))
-
+    while(cardAreaZone3.childElementCount != 3){
+        cardAreaZone1.appendChild(createNewCardElement(gameObject.dealNewCardlevel(3)))
+        }
     //Draw noble tokens
-    for(let i = 0; i < testGame.playerCount; i++){
-        
+    for(let i = 0; i <= gameObject.playerCount; i++){
+        createNobleCardAddToNobleZone(gameObject.dealNewNobleCard())
     }
     //initialize global tokens
+    const startingResources = {
+        "resourceG": gameObject.tokens.Green,
+        "ResourceR": gameObject.tokens.Red,
+        "ResourceU": gameObject.tokens.Blue,
+        "ResourceB": gameObject.tokens.Black,
+        "ResourceW": gameObject.tokens.White,
+        "ResourceY": gameObject.tokens.Yellow
+    }
+    updateResourceNumbers(startingResources)
+}
+/**
+ * 
+ * @param {*} nobleInstance 
+ */
+function createNobleCardAddToNobleZone(nobleInstance) {
+    // Create the main container for the noble card
+    const nobleCardContainer = document.createElement('div');
+    nobleCardContainer.classList.add('nobleCardContainer');
 
+    // Map resource colors to their corresponding values in the noble instance
+    const resourceColors = {
+        'Black': nobleInstance.Black,
+        'Blue': nobleInstance.Blue,
+        'Green': nobleInstance.Green,
+        'Red': nobleInstance.Red,
+        'White': nobleInstance.White
+    };
+
+    // Iterate over each resource color and add elements if the requirement is greater than 0
+    for (const color in resourceColors) {
+        const requirement = resourceColors[color];
+        if (requirement > 0) {
+            const nobleRequire = document.createElement('div');
+            nobleRequire.classList.add('nobleRequire', `color${color.charAt(0).toUpperCase()}`);
+            nobleRequire.textContent = requirement;
+            nobleCardContainer.appendChild(nobleRequire);
+        }
+    }
+
+    // Append the noble card container to the div with ID NobleZone
+    const nobleZone = document.getElementById('NobleZone');
+    if (nobleZone) {
+        nobleZone.appendChild(nobleCardContainer);
+    } else {
+        console.error('Element with ID NobleZone not found.');
+    }
+}
+
+/**
+ * format input such that:
+ * const newNumbers = {
+    "resourceG": X,
+    "ResourceR": X,
+    "ResourceU": X,
+    "ResourceB": X,
+    "ResourceW": X,
+    "ResourceY": X
+    };
+ * @param {*} newNumbers 
+ */
+function updateResourceNumbers(newNumbers) {
+    // Iterate over each resource ID
+    Object.keys(newNumbers).forEach(resourceId => {
+        // Find the div element with the corresponding ID
+        const resourceDiv = document.getElementById(resourceId);
+        if (resourceDiv) {
+            // Update the number inside the div
+            resourceDiv.querySelector('div').textContent = newNumbers[resourceId];
+        }
+    });
 }
 
 /**
